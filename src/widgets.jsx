6677d0d1bca9d05291451c4843f4ef50669c0430
hillarioh <okerioh@gmail.com>
2020-09-16 12:41:06 +0300

    import and add clock component

--- src/widgets.jsx
+++ src/widgets.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import {Clock} from './clock';
 
 const Root = () => {
-        return (<h1>Am here</h1>);
+        return (
+            <Clock />
+        );
 }
 
 
